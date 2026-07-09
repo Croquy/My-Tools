4d1d20f02f6e4c754f594067a51c3c44fee46cff
Croquy <86916937+Croquy@users.noreply.github.com>
2026-07-09 11:43:05 +0200

    Update PASSWORD_HASH with a new hash value

--- gate.js
+++ gate.js
@@ -1,6 +1,6 @@
 (function(){
   var STORAGE_KEY = 'mytools_unlocked_v1';
-  var PASSWORD_HASH = 'COLLE_TON_HASH_ICI';
+  var PASSWORD_HASH = '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918';
 
   if (localStorage.getItem(STORAGE_KEY) === 'true') return;
 
